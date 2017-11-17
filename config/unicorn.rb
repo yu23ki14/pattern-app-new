9d1a2f51a73c69f1944a63bc8fe8486069b232e1
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-11-17 08:56:29 +0000

    modifi unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,27 @@
-# -*- coding: utf-8 -*-
+APP_PATH = "/home/manabu/patternapp"
+
 worker_processes 3
-
-listen  Dir.pwd + '/tmp/unicorn.sock'
-pid     Dir.pwd + '/tmp/unicorn.pid'
-
-log = Dir.pwd + '/log/unicorn.log'
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+working_directory APP_PATH
+listen "/var/run/unicorn/unicorn.socket"
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+stderr_path APP_PATH + "/log/unicorn.log"
+stdout_path APP_PATH + "/log/unicorn.log"
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
-defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  ActiveRecord::Base.connection.disconnect!
 
-old_pid = "#{ server.config[:pid] }.oldbin"
-unless old_pid == server.pid
-  begin
-   sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-   Process.kill :QUIT, File.read(old_pid).to_i
-   rescue Errno::ENOENT, Errno::ESRCH
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
   end
-end
 end
 
 after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
