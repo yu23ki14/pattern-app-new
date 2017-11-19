a7dd88b8519c8b11cce0ac43dc91a03e1da4524f
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-11-19 05:55:02 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require "csv"
 
-CSV.foreach('db/csv/languages.csv') do |row|
-  Language.create(:lg_name => row[0], :lg_code => row[1])
-end
+#CSV.foreach('db/csv/languages.csv') do |row|
+#  Language.create(:lg_name => row[0], :lg_code => row[1])
+#end
 
-CSV.foreach('db/csv/patterns.csv') do |row|
-  Pattern.create(:language_id => row[0], :cat_code => row[1], :cat_code_24 => row[2], :pattern_no => row[3], :pattern_name => row[4], :summary => row[5], :context => row[6], :b_problem => row[7], :problem => row[8], :b_solution => row[9], :solution => row[10], :consequence => row[11])
-end
+#CSV.foreach('db/csv/patterns.csv') do |row|
+#  Pattern.create(:language_id => row[0], :cat_code => row[1], :cat_code_24 => row[2], :pattern_no => row[3], :pattern_name => row[4], :summary => row[5], :context => row[6], :b_problem => row[7], :problem => row[8], :b_solution => row[9], :solution => row[10], :consequence => row[11])
+#end
