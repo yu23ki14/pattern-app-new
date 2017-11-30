4e9162ff3725e03bd52f1ae40a3916df92ae9e25
yuki kawabe <yuki_021423@yahoo.co.jp>
2017-11-30 12:30:49 +0000

    add project models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114224556) do
+ActiveRecord::Schema.define(version: 20171130075626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,16 @@ ActiveRecord::Schema.define(version: 20171114224556) do
     t.datetime "updated_at", null: false
     t.index ["language_id"], name: "index_practices_on_language_id"
     t.index ["user_id"], name: "index_practices_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "project_name"
+    t.string "project_summary"
+    t.string "project_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "recommends", force: :cascade do |t|
