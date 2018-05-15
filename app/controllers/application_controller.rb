2a76cea84da9c22576be7d2f066fdd66ce42e6ad
yuki kawabe <yuki_021423@yahoo.co.jp>
2018-05-15 15:42:55 +0000

    english base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_user
   before_action :backhome
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
@@ -23,6 +24,13 @@ class ApplicationController < ActionController::Base
     def set_user
       if user_signed_in?
         @user = User.find(current_user.id)
+      end
+    end
+    def set_locale
+      if user_signed_in?
+        params[:locale] = @user.locale
+      else
+        params[:locale] = "ja"
       end
     end
 
