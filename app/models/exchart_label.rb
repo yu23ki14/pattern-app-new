adf394c0b0c564fc823eb23634a14c7a04128b2d
yuki kawabe <yuki_021423@yahoo.co.jp>
2018-07-03 07:42:32 +0000

    fix transrasion on register new

--- app/models/exchart_label.rb
+++ app/models/exchart_label.rb
@@ -4,9 +4,9 @@ class ExchartLabel < ApplicationRecord
   #update exchart_labels set label_ja = E'コア,勢いをもつ,まとまり,積極性,深い絆,創造の環境,発見の連鎖,実現へ加速,こだわり,つくり込み,継続の力' where language_id = 2
   #update exchart_labels set label_ja = E'コア,認識の共有,魅力的な\n展開,仕上げ,実感を\n生む工夫,つくり込み,能動性の誘発,準備,語り手,つながり,究極' where language_id = 3
 
-  #update exchart_labels set label_en = E'core,begining,practicing,chain\nlearning,develop,action,abduction,process,finishing,fellow,influence,reflection,uniqueness' where language_id = 1
-  #update exchart_labels set label_en = E'core,vigor,cohensive,activeness,bond,environment,nexus,accelerate,particular,fabricate,continuation' where language_id = 2
-  #update exchart_labels set label_en = E'core,sharing recognition,attractive,finishing,real feeling,build up,activeness,preparation,storyteller,connection,ultimate' where language_id = 3
+  #update exchart_labels set label_en = E'Core,Begining,Practical\nlearning,Chain\nlearning,Drill,Action,Imagination,Creativity,Finishing,Fellow,Influence,Reflection,Uniqueness' where language_id = 1
+  #update exchart_labels set label_en = E'Core,Dynamic,Unity,Activeness,Bond,Environment,Nexus,Acceleration,Quality,Elaboration,Continuation' where language_id = 2
+  #update exchart_labels set label_en = E'Core,Shared understanding,Attraction,Finishing,Reality,Elaboration,Remarkable,Preparation,Storyteller,Connection,Ultimate' where language_id = 3
 
   def label
     self.send("label_#{I18n.locale}")
